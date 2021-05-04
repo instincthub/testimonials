9efa737745f1933e005851e36ca69949ba61a85f
Noah Olatoye <noaholatoye@gmail.com>
2021-05-04 01:45:39 +0100

    Updated Js File

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,25 +1,28 @@
 const testimonials = document.querySelectorAll('.user_meta')
 
 
-// Used the forEach loop add 'open' class to the profile div.
-// Used the for loop to remove 'open' class from the profile div. 
+// Use the forEach Loop to add 'open' class to the profile div.
+// Use the for loop to remove the 'open' class form the profile div. 
 
 testimonials.forEach(item =>{
     item.addEventListener('click', e =>{
         const element = e.target
 
-        for (let i = 0; i < testimonials.length; i++){
-            testimonials[i].parentElement.classList.remove('open')
+        // Remove 'open' class from profile div.
+        for (let i = 0; i < testimonials.length; i++) {
+            testimonials[i].parentElement.classList.remove('open')   
         }
 
+        // Check button is click
         if (element.classList == 'user_meta'){
             element.parentElement.classList.add('open')
         }
-        else if (element.tagName == 'H3' || element.tagName == 'P') {
+        else if(element.tagName == 'H3' || element.tagName == 'P'){
             element.parentElement.parentElement.parentElement.classList.add('open')
         }
         else if (element.tagName == 'IMG' || element.children[1]){
             element.parentElement.parentElement.classList.add('open')
+
         }
     })
 })
